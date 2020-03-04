c9547fe8da1db30c57df8bf0acdbff7ba469aca3
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-04 09:59:46 -0700

    Test Photograph exists and modify runner file

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,4 +1,10 @@
 require "CSV"
+require './lib/photograph'
 
-CSV.foreach('.data/photograph.csv') do |row|
-end  
+CSV.foreach('.data/photograph.csv', headers: true, header_converters: :symbol) do |row|
+  id = row[:id].to_s
+  name = row[:name]
+  artist_id = row[:artist_id].to_s
+  year = row[:year].to_s
+  photograph = Photograph.new(id, name, artist_id, year)
+end
